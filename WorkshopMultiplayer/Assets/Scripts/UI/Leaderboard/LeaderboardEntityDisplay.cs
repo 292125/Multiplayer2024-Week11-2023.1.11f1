52dfe5297b2923d6c2a265aa716f592708d4660c
292125 <85726099+292125@users.noreply.github.com>
2024-03-21 10:03:39 +0700

    Week11

--- WorkshopMultiplayer/Assets/Scripts/UI/Leaderboard/LeaderboardEntityDisplay.cs
+++ WorkshopMultiplayer/Assets/Scripts/UI/Leaderboard/LeaderboardEntityDisplay.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Collections;
 using UnityEngine;
+using Unity.Netcode;
 
 public class LeaderboardEntityDisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_Text displayText;
+    [SerializeField] private Color myColor;
+
+    private FixedString32Bytes playerName;
+    
+    public ulong ClientId { get; private set; }
+    public int Coins { get; private set; }
+
+    public void Initailise(ulong clientId, FixedString32Bytes playerName, int coins)
     {
-        
+        ClientId = clientId;
+        this.playerName = playerName;
+
+        if (clientId == NetworkManager.Singleton.LocalClientId)
+        {
+            displayText.color = myColor;
+        }
+        UpdateCoins(coins);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateCoins(int coins)
     {
-        
+        Coins = coins;
+        UpdateText();
     }
+
+    public void UpdateText()
+    {
+        displayText.text = $"{transform.GetSiblingIndex()+1}. {playerName} ({Coins})";
+    }
+
 }
